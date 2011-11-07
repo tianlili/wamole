40e3958a1df5d5c08a8108b7f55c4a958a9d0bb3
tianlili <tianlili@b000000020050.internal.baidu.com>
2011-11-07 15:51:04 +0800

    提交页面相关文件

---
+++ src/main/resources/ftl/pages/php/post-json.php
@@ -0,0 +1,25 @@
+<?php
+	echo '
+	{
+"page":1,
+"total":3,
+"rows":[
+{
+"id":"001",
+"cell":{
+"name":"Tangram-base",
+"path":"D:/Workplace/Tangram-base",
+"rule":"Zimbabwe"
+}
+},
+{
+"id":"002",
+"cell":{
+"name":"Tangram-component",
+"path":"D:/Workplace/Tangram-component",
+"rule":"Zimbabwe"
+}
+}
+]
+}'
+?>
