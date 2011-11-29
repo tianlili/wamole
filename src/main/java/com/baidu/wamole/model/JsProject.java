a41ab2e142657ceab8edade35e2b2c96e70bc053
bellcliff <cliff_bell.cn@hotmail.com>
2011-11-29 16:39:32 +0800

    写死执行页

--- src/main/java/com/baidu/wamole/model/JsProject.java
+++ src/main/java/com/baidu/wamole/model/JsProject.java
@@ -2,7 +2,6 @@ package com.baidu.wamole.model;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
